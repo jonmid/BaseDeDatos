46d2f7bd200e1512400b9b3cdd1f8ec218ccde13
JonMid <jamideros@gmail.com>
2017-10-24 14:53:40 -0500

    Cambios interfaz

--- app/src/main/java/com/example/jonmid/basededatos/RegisterActivity.java
+++ app/src/main/java/com/example/jonmid/basededatos/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.jonmid.basededatos;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 import com.example.jonmid.basededatos.Connection.Sqlite;
 import com.example.jonmid.basededatos.Utilities.Utilities;
+import com.example.jonmid.basededatos.Views.DetailActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -46,5 +49,33 @@ public class RegisterActivity extends AppCompatActivity {
         Long idResult = db.insert(Utilities.TABLA_NAME, Utilities.TABLA_FIELD_ID, values);
 
         Toast.makeText(this, "Id registro: "+idResult, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSearchUser(View view){
+        searchUser();
+    }
+
+    public void searchUser(){
+        Sqlite sqlite = new Sqlite(this, "bd_users", null, 1);
+        SQLiteDatabase db = sqlite.getReadableDatabase();
+
+        String[] params = {textInputEditTextName.getText().toString()};
+        String[] fields = {Utilities.TABLA_FIELD_NAME,Utilities.TABLA_FIELD_PHONE,Utilities.TABLA_FIELD_EMAIL};
+
+
+        try {
+            Cursor cursor = db.query(Utilities.TABLA_NAME, fields, Utilities.TABLA_FIELD_NAME+"=?",params,null,null,null);
+            cursor.moveToFirst();
+
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra("name", cursor.getString(0));
+            intent.putExtra("phone", cursor.getString(1));
+            intent.putExtra("email", cursor.getString(2));
+            startActivity(intent);
+
+            cursor.close();
+        }catch (Exception e){
+            Toast.makeText(this, "Nombre del usuario no encontrado", Toast.LENGTH_SHORT).show();
+        }
     }
 }
